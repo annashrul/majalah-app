d120d6899572e9b168cbb7f19ae7a02fb54628ad
dayuss <ddtardian@gmail.com>
2019-08-12 16:28:02 +0700

    merge

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','session','pagination','form_validation','encrypt','email');
+$autoload['libraries'] = array('database','session','pagination','form_validation','email');
 
 /*
 | -------------------------------------------------------------------
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('m_crud','m_website','m_export_file');
+$autoload['model'] = array('m_crud','m_website');
